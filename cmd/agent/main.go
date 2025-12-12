1f4e063592d79a3a9dc3d891f0344a8c63630ec4
DaGyeong Kim <65109465+S0okJu@users.noreply.github.com>
2025-12-12 15:22:01 +0900

    feat: Add unit test code for internal/agent/run.go (#7)
    * feat: Make test codes for internal/agent/run.go(#5)
    
    - Move run() function in main.go to internal/agent/run.go
    - Create run_test.go code
    - Make workflows for testing unit test.
    
    * fix: Fix unit test path(#5)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,70 +2,22 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 
+	"github.com/bpfstack/bpfstack/internal/agent"
 	"github.com/bpfstack/bpfstack/pkg/agent/core"
-	fileopen "github.com/bpfstack/bpfstack/pkg/probes/fileopen"
+	"github.com/bpfstack/bpfstack/pkg/probes/fileopen"
 	"github.com/bpfstack/bpfstack/pkg/probes/helloworld"
 )
 
 func main() {
-    if err := run(); err != nil {
-        fmt.Printf("Error: %v", err)
-        os.Exit(1)
+    factories := map[string]core.ProbeFactoryFunc{
+        "helloworld": helloworld.New,
+        "fileopen": fileopen.New,
     }
-}
-
-// run executes the agent logic.
-func run() error {
-    // If sigint(ctrl+c) or sigterm(kill) is received,
-    // the context is done and the program should exit.
-    ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-    defer stop()
-
-    dataChan := make(chan core.TelemetryEvent, 100) 
-    errChan := make(chan error, 100)
-    
-    fmt.Println("Starting the probe manager")
-    probeMgr := core.NewProbeManager(dataChan, errChan)
-    probeMgr.Register("helloworld", helloworld.New)
-    probeMgr.Register("fileopen", fileopen.New)
-
-    currentConfig := map[string]bool{
-        "fileopen":   true,
-        "helloworld": true,
-    }
-
-    // Receive the data in a separate goroutine.
-    // If an error occurs during reconcile,
-    // the error is sent to the errChan.
-    go func() {
-        for {
-            select {
-            case event := <-dataChan:
-                fmt.Printf("[%s] %s\n", event.ProbeName, event.Data) 
-            case err := <-errChan:
-                fmt.Printf("Error: %v\n", err)
-            case <-ctx.Done():
-                return
-            }
-        }
-    }()
-
-    // Reconcile the probes.
-    probeMgr.Reconcile(ctx, currentConfig)
-    
-    fmt.Println("Agent started") 
-
-    // Wait for the context to be done.
-    <-ctx.Done()
-    
-    fmt.Println("Shutting down the agent")
-    probeMgr.Shutdown()
-    
-    return nil
+	if err := agent.Run(factories); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
 }
